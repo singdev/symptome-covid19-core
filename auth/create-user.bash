7ea446a4b5cac6a78235bd1487d89c5cb68b433f
Nve Hounkponou Orphée <norpheehounnkponou@gmail.com>
2020-04-22 01:55:52 +0100

    save

--- auth/create-user.bash
+++ auth/create-user.bash
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-curl -H "Accept: application/json" -H "Content-Type: application/json" -d @root.json http://localhost:3001/users/auth/root >> token.txt
+curl -H "Accept: application/json" -H "Content-Type: application/json" -d @root.json http://54.38.190.167:20022/users/auth/root >> token.txt
 
-token=`cat token.txt`;
+token=`cat token.txt`
+echo $token
 
-curl -H "Accept: application/json" -H "Content-Type: application/json" -H "authorization: Bearer ${token}" -d @rufus.json http://localhost:3001/users
+curl -H "Accept: application/json" -H "Content-Type: application/json" -H "authorization: Bearer ${token}" -d @rufus.json http://54.38.190.167:20022/users
 
 rm token.txt
